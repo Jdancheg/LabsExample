f7a87cc84048274829b46c392181d8ca7cf0f9ef
Alexey <alex_128@mail.ru>
2022-10-10 13:46:25 +0300

    111

---
+++ AllLabsApp/AllLabsApp/ILabs.cs
@@ -0,0 +1,27 @@
+﻿namespace AllLabsApp;
+
+internal interface ILabs
+{
+    /// <summary>
+    /// Метод демонстрации отработки всех заданий лабораторной работы
+    /// </summary>
+    void Demo();
+
+    /// <summary>
+    /// Заголовок лабораторной работы
+    /// </summary>
+    /// <returns></returns>
+    string Title();
+
+    /// <summary>
+    /// Описание заданий
+    /// </summary>
+    /// <returns></returns>
+    string Description();
+
+    /// <summary>
+    /// Идентификатор (номер) лабораторной работы
+    /// </summary>
+    /// <returns></returns>
+    int Id();
+}
---
+++ AllLabsApp/AllLabsApp/Lab1/Lab1.cs
@@ -0,0 +1,38 @@
+﻿namespace AllLabsApp;
+
+public class Lab1 : ILabs
+{
+    /// <summary>
+    /// Метод демонстрации всех заданий лабораторной работы
+    /// </summary>    
+    public void Demo()
+    {
+        // Console.WriteLine("Демо");
+        // ход лабы
+    }
+    /// <summary>
+    /// Метод возвращает описание заданий лабораторной работы
+    /// </summary>
+    /// <returns></returns>
+    /// <returns>Описание лабы</returns>
+    public string Description()
+    {
+        return "Задания лабы №1";
+    }
+    /// <summary>
+    /// Метод возвращает номер лабораторной работы
+    /// </summary>
+    /// <returns>Номер лабы</returns>
+    public int Id()
+    {
+        return 1;
+    }
+    /// <summary>
+    /// Метод возвращает заголовок лабораторной работы
+    /// </summary>
+    /// <returns></returns>
+    public string Title()
+    {
+        return "Лабораторная №1. Тема: _____";
+    }
+}
